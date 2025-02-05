07fa44598faddb3462c22b84f5cbb08a40e4c28c
Ian Restrepo <ianrpoecomco@gmail.com>
2025-02-05 00:55:17 -0500

    ADD:Front-End | Dashboard - Sidebar toFix: SidebarFunction

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,12 @@
 export type ProjectTitle = string;
 export type ProjectLanguage = string;
 export type ProjectFavicon = string;
+
+export const DashboardLinks = [
+    { icon: 'Sidebar/home', label: 'Home', href: '/dashboard'},
+    { icon: 'Sidebar/users', label: 'Users', href: '/dashboard/users'},
+    { icon: 'Sidebar/logs', label: 'Logs', href: '/dashboard/logs'},
+    { icon: 'Sidebar/billing', label: 'Billing', href: '/dashboard/billing'},
+    { icon: 'Sidebar/settings', label: 'Settings', href: '/dashboard/settings'},
+    { icon: 'Sidebar/feedback', label: 'Give Feedback', href: '/dashboard/feedback'},
+]
